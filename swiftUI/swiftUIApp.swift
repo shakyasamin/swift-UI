240a25f3dc53e7d4a62793e0a04345d5e6b8dbce
saminshakya <saminshakya27@gmail.com>
2023-05-24 09:57:36 +0545

    project created

---
+++ swiftUI/swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIApp.swift
+//  swiftUI
+//
+//  Created by MicroBanker Nepal Pvt. Ltd. on 24/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
